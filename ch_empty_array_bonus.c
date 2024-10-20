7c285773148e416b5b81eb9234368a33dd1e53ee
kurrrru <kurrrru8850@gmail.com>
2024-10-20 22:52:01 +0900

    update

--- ch_empty_array_bonus.c
+++ ch_empty_array_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nkawaguc <nkawaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 13:04:29 by nkawaguc          #+#    #+#             */
-/*   Updated: 2024/10/20 13:04:30 by nkawaguc         ###   ########.fr       */
+/*   Updated: 2024/10/20 22:51:50 by nkawaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,5 @@
 
 void	empty_array(void)
 {
-	char	*buf[1];
-	int		read_size;
-
-	read_size = read(STDIN_FILENO, buf, 1);
-	if (read_size == 0)
-	{
-		ft_putendl_fd(MSG_OK, STDOUT_FILENO);
-		exit(SUCCESS);
-	}
-	else
-	{
-		ft_putendl_fd(MSG_KO, STDOUT_FILENO);
-		exit(FAILURE);
-	}
+	exit(SUCCESS);
 }
